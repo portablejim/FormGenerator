de89c82c4b4fb815b2e25fa59801779148edeb9a
portablejim <dcseee-github@yahoo.com.au>
2015-10-02 11:18:14 +1000

    Add text element.

---
+++ src/formgenerator/formelements/TextElement.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: james
+ * Date: 2/10/15
+ * Time: 10:03 AM
+ */
+
+namespace formgenerator\formelements;
+
+
+use formgenerator\IFormElement;
+
+class TextElement implements IFormElement
+{
+    protected $value;
+    /**
+     * @var
+     */
+    private $name;
+    /**
+     * @var
+     */
+    private $width;
+    /**
+     * @var
+     */
+    private $promptTextId;
+    /**
+     * @var
+     */
+    private $errorTextId;
+    /**
+     * @var
+     */
+    private $required;
+
+    public function __construct($name, $width, $promptTextId, $errorTextId, $required)
+    {
+
+        $this->name = $name;
+        $this->width = $width;
+        $this->promptTextId = $promptTextId;
+        $this->errorTextId = $errorTextId;
+        $this->required = $required;
+        $this->value = "";
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    public function fillData($data)
+    {
+        $this->value = $data;
+    }
+
+    public function isValid()
+    {
+        if(strlen($this->value) == 0 && $this->required) {
+            return false;
+        }
+
+        if(strlen($this->value) >= 1000) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function getErrorMessage()
+    {
+        return $this->errorTextId;
+    }
+
+    public function getDataArray()
+    {
+        $output = array(
+            "name" => $this->name,
+            "type" => "text",
+            "width" => $this->width,
+            "height" => 1,
+            "promptId" => $this->promptTextId,
+            "required" => $this->required,
+            "value" => $this->value
+        );
+        return $output;
+    }
+}
---
+++ test/TextElementTest.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: james
+ * Date: 2/10/15
+ * Time: 10:06 AM
+ */
+
+namespace testForms;
+
+include_once "../src/formgenerator/IFormElement.php";
+include_once "../src/formgenerator/formelements/TextElement.php";
+
+use formgenerator\formelements\TextElement;
+
+class TextElementTest extends \PHPUnit_Framework_TestCase
+{
+    function testBasicDataValidArray()
+    {
+        $textElem = new TextElement("testName", 1, "prompt", "error", false);
+
+        $testData = array(
+            "name" => "testName",
+            "type" => "text",
+            "width" => 1,
+            "height" => 1,
+            "promptId" => "prompt",
+            "required" => false,
+            "value" => ""
+        );
+
+        $this->assertSame($testData, $textElem->getDataArray());
+    }
+
+    function testName()
+    {
+        $textElem = new TextElement("testName", 1, "prompt", "error", false);
+
+        $this->assertSame("testName", $textElem->getName());
+    }
+
+    function testValue()
+    {
+        $textElem = new TextElement("testName", 1, "prompt", "error", false);
+
+        $this->assertSame("", $textElem->getValue());
+    }
+
+    function testErrorMessage()
+    {
+        $textElem = new TextElement("testName", 1, "prompt", "error", false);
+
+        $this->assertSame("error", $textElem->getErrorMessage());
+    }
+
+    function testFillForm()
+    {
+        $textElem = new TextElement("testName", 1, "prompt", "error", false);
+        $textElem->fillData("dummyData");
+
+        $this->assertSame("dummyData", $textElem->getValue());
+    }
+
+    function testValidIfEmptyAndNotRequired()
+    {
+        $textElem = new TextElement("testName", 1, "prompt", "error", false);
+
+        $this->assertTrue($textElem->isValid());
+    }
+
+    function testInvalidIfEmptyAndRequired()
+    {
+        $textElem = new TextElement("testName", 1, "prompt", "error", true);
+
+        $this->assertFalse($textElem->isValid());
+    }
+
+    function testValidIfFilledAndRequired()
+    {
+        $textElem = new TextElement("testName", 1, "prompt", "error", true);
+        $textElem->fillData("testData");
+
+        $this->assertTrue($textElem->isValid());
+    }
+
+    function testValidIfFilledWithLongText()
+    {
+        $longText =
+              "This text is 00999 long ----------------------------------------------------------------------------"
+            . "----------------------------------------------------------------------------------------------------"
+            . "----------------------------------------------------------------------------------------------------"
+            . "----------------------------------------------------------------------------------------------------"
+            . "----------------------------------------------------------------------------------------------------"
+            . "----------------------------------------------------------------------------------------------------"
+            . "----------------------------------------------------------------------------------------------------"
+            . "----------------------------------------------------------------------------------------------------"
+            . "----------------------------------------------------------------------------------------------------"
+            . "---------------------------------------------------------------------------------------------------";
+
+        $textElem1 = new TextElement("testName", 1, "prompt", "error", true);
+        $textElem1->fillData($longText);
+        $textElem2 = new TextElement("testName", 1, "prompt", "error", true);
+        $textElem2->fillData($longText);
+
+        $this->assertTrue($textElem1->isValid());
+        $this->assertTrue($textElem2->isValid());
+    }
+
+    function testInvalidIfTooLongFilled()
+    {
+        $longText =
+            "This text is 00999 long ----------------------------------------------------------------------------"
+            . "----------------------------------------------------------------------------------------------------"
+            . "----------------------------------------------------------------------------------------------------"
+            . "----------------------------------------------------------------------------------------------------"
+            . "----------------------------------------------------------------------------------------------------"
+            . "----------------------------------------------------------------------------------------------------"
+            . "----------------------------------------------------------------------------------------------------"
+            . "----------------------------------------------------------------------------------------------------"
+            . "----------------------------------------------------------------------------------------------------"
+            . "----------------------------------------------------------------------------------------------------";
+
+        $textElem1 = new TextElement("testName", 1, "prompt", "error", true);
+        $textElem1->fillData($longText);
+        $textElem2 = new TextElement("testName", 1, "prompt", "error", true);
+        $textElem2->fillData($longText);
+
+        $this->assertFalse($textElem1->isValid());
+        $this->assertFalse($textElem2->isValid());
+    }
+}
